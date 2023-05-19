a414f16fc908beb1e84d295baa22075a032cd6ee
khalid <khalid.ibr212@gmail.com>
2023-05-19 14:13:58 +0300

    update 003

---
+++ lib/pages/Quran_Page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class selectedPage extends StatelessWidget {
+  const selectedPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('s'),
+      ),
+    );
+  }
+}
